3ee709b0fd619d8fca8007d7599164093f1c65f7
weiwang314 <wei.wang@wbdevops.com>
2017-10-19 17:53:09 -0700

    fix styles

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import App from '../../src/components/app';
 import './assets/styles/main.scss';
-import '../../src/components/app/styles.scss';
+
+import App from '../../src/components/app';
 import routes from './config/routes';
 import nav from './config/main-nav.json';
 import store from './config/store';
