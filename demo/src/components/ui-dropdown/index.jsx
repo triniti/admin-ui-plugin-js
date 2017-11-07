7ecb3a505348f4ffe5c04de0c6ea4180e34866b2
Matt Gollnick <mgollnick@gmail.com>
2017-11-07 11:21:30 -0800

    RE #35: Moved trinary-control from ui-dropdown to ui-form

--- demo/src/components/ui-dropdown/index.jsx
+++ demo/src/components/ui-dropdown/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardBody, Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Row, TrinaryControl } from '../../../../src/components';
+import { Card, CardBody, Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Row } from '../../../../src/components';
 import CardHeader from '../../../../src/components/card-header/index';
 
 class UiDropdown extends React.Component {
@@ -30,60 +30,27 @@ class UiDropdown extends React.Component {
 
   render() {
     return (
-      <div>
-        <Card>
-          <CardHeader>Dropdown</CardHeader>
-          <CardBody spacing="lg">
-            <Row>
-              <Col xs="6">
-                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} className="mb-4">
-                  <DropdownToggle caret color="default" outline>
-                    Dropdown
-                  </DropdownToggle>
-                  <DropdownMenu arrow="right" right>
-                    <DropdownItem header>Header</DropdownItem>
-                    <DropdownItem disabled>Action</DropdownItem>
-                    <DropdownItem>Another Action</DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>Another Action</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-              </Col>
-            </Row>
-          </CardBody>
-        </Card>
-
-        <Card>
-          <CardHeader>Trinary Control - Default</CardHeader>
-          <CardBody spacing="lg">
-            <Row>
-              <Col xs="3" xs="4">
-                <div style={{width: '4rem'}}>
-                  <TrinaryControl name="trinaryDefault" onChange={this.handleChange} value={this.state.trinaryDefault} />
-                </div>
-              </Col>
-              <Col xs="6">
-                <span>Selected Value: {`${this.state.trinaryDefault}`}</span>
-              </Col>
-            </Row>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardHeader>Trinary Control - Custom</CardHeader>
-          <CardBody spacing="lg">
-            <Row>
-              <Col xs="3" xs="4">
-                <div style={{width: '105px'}}>
-                  <TrinaryControl name="trinaryCustom" unsetText="Any User" trueText="Is Staff" falseText="Non Staff" radius="round" size="sm" value={this.state.trinaryCustom} onChange={this.handleChange} />
-                </div>
-              </Col>
-              <Col xs="6">
-                <span>Selected Value: {`${this.state.trinaryCustom}`}</span>
-              </Col>
-            </Row>
-          </CardBody>
-        </Card>
-      </div>
+      <Card>
+        <CardHeader>Dropdown</CardHeader>
+        <CardBody spacing="lg">
+          <Row>
+            <Col xs="6">
+              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} className="mb-4">
+                <DropdownToggle caret color="default" outline>
+                  Dropdown
+                </DropdownToggle>
+                <DropdownMenu arrow="right" right>
+                  <DropdownItem header>Header</DropdownItem>
+                  <DropdownItem disabled>Action</DropdownItem>
+                  <DropdownItem>Another Action</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Another Action</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
     );
   }
 }
