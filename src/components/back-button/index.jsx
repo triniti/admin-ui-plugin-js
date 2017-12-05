c8977fef1d6a3b8178bd556424fb4a6f7a38693f
weiwang314 <wei.wang@wbdevops.com>
2017-12-05 15:57:40 -0800

    delete buttons

--- src/components/back-button/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from '../button';
-import Icon from '../icon';
-
-export default props => (
-  <Button size="sm" className="mb-0" action="back" {...props}>
-    <Icon imgSrc="back" alt="back" size="xs" />Back
-  </Button>
-);
