f43308e320e17cf7c1acd4ad23e46251da8dd81b
Matt Gollnick <mgollnick@gmail.com>
2018-01-24 17:43:36 -0800

    RE #53: Added split parameter to main-content

--- src/components/main-content/index.jsx
+++ src/components/main-content/index.jsx
@@ -1,25 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { mapToCssModules } from '../utils';
 import './styles.scss';
 
-const MainContent = ({ size, children }) => (
-  <div className="main-content">
-    {size &&
-    <div className={`max-width max-width-${size}`}>
-      {children}
-    </div>
-    }
-    {!size && children}
-  </div>
-);
-
-MainContent.propTypes = {
+const propTypes = {
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
   size: PropTypes.string,
+  split: PropTypes.bool,
   children: PropTypes.node,
+  className: PropTypes.string,
+  cssModule: PropTypes.object,
 };
 
-MainContent.defaultProps = {
+const defaultProps = {
+  tag: 'div',
   size: '',
 };
+
+const MainContent = (props) => {
+  const {
+    className,
+    cssModule,
+    size,
+    split,
+    children,
+    tag: Tag,
+    ...attributes
+  } = props;
+  const classes = mapToCssModules(classNames(
+    className,
+    'main-content',
+    split ? 'main-content-split' : false,
+  ), cssModule);
+
+  return (
+    <Tag {...attributes} className={classes}>
+        {size &&
+        <div className={`max-width max-width-${size}`}>
+          {children}
+        </div>
+        }
+        {!size && children}
+    </Tag>
+  );
+};
+
+MainContent.propTypes = propTypes;
+MainContent.defaultProps = defaultProps;
 
 export default MainContent;
