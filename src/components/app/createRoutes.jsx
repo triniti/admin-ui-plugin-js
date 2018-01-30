03f5f15bd7e177ba77a8383afe72151e4f40661b
Richard Sumilang <rsumilang@users.noreply.github.com>
2018-01-29 17:08:04 -0800

    Lazy loading (#55)
    * Universal component support
    
    * Adding react universal component
    
    * Updating version
    
    * I like this way more
    
    * - Adding support for async public paths
    - Adding support for async options based on route
    - Simplifying universal component creation
    
    * Adding load component to universal component
    
    * Overriding eslint max length
    
    * Linting
    
    * Linting
    
    * More short handing and linting
    
    * Returning earlier.
    
    * Adding more documentation

--- src/components/app/createRoutes.jsx
+++ src/components/app/createRoutes.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import universal from 'react-universal-component';
+import Loading from '../loading';
 
-export default function createRoutes(routes, authHoc = null) {
-  return Object.values(routes).map((route) => {
+/**
+ * Chooses to return either a standard component if `route.eager` is set to true or a universal
+ * component for a lazy loaded component.
+ *
+ * If universal component, then `route.component` must be a method or import (promise).
+ *
+ * Ie: import('somepath/component');
+ * Ie: (props) => import('somepath/component');
+ *
+ * @param {Boolean|undefined} [route.eager]
+ * @param {React.Component|function} route.component
+ * @returns React.Component
+ * @see https://github.com/faceyspacey/react-universal-component
+ */
+const getComponent = (route) => {
+  if (route.eager) {
+    return route.component;
+  }
+
+  return universal(route.component, Object.assign({ loading: Loading }, route.async || {}));
+};
+
+export default (routes, authHoc = null) =>
+  Object.values(routes).map((route) => {
     if (route.redirect) {
       return (
         <Route
@@ -14,9 +38,12 @@ export default function createRoutes(routes, authHoc = null) {
       );
     }
 
+    // Either standard or universal component
+    const component = getComponent(route);
+
     // handle public routes (no auth required, e.g: login)
     if (route.public || !authHoc) {
-      return <Route key={route.path} {...route} />;
+      return <Route key={route.path} {...route} component={component} />;
     }
 
     // handle private routes (default value, most pages in cms)
@@ -26,8 +53,7 @@ export default function createRoutes(routes, authHoc = null) {
         exact={route.exact || true}
         strict={route.strict || false}
         path={route.path}
-        component={authHoc(route.component, route.extra || {})}
+        component={authHoc(component, route.extra || {})}
       />
     );
   });
-}
