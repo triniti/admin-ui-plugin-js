2315ab7ccd9dca79389755158e4d0f6e92640188
Matt Gollnick <mgollnick@gmail.com>
2018-02-05 11:51:32 -0800

    RE #58: Badges are clean

--- demo/src/screens/badges/index.jsx
+++ demo/src/screens/badges/index.jsx
@@ -20,7 +20,7 @@ const UiBadge = () => (
         <h2>H2 Heading .h2 <Badge color="primary">New</Badge></h2>
         <h3>H3 Heading .h3 <Badge color="success" pill>New</Badge></h3>
         <h4>H4 Heading .h4 <Badge color="info">New</Badge></h4>
-        <h5>H5 Heading .h5 <Badge color="warning">New</Badge></h5>
+        <h5>H5 Heading .h5 <Badge color="danger">New</Badge></h5>
         <h6>h6 Heading .h6 <Badge pill>New</Badge></h6>
 
         <CardTitle tag="h4">Variations</CardTitle>
@@ -49,7 +49,7 @@ const UiBadge = () => (
         <Button outline radius="round" className="mr-3">outline <Badge color="primary" alert>3</Badge></Button>
         <Button color="info" radius="round" className="mr-3">info <Badge color="warning" alert>0</Badge></Button>
         <Button className="mr-3">inline<Badge color="danger" inline>24</Badge></Button>
-        <Button className="mr-3">inline pill<Badge color="danger" inline pill>24</Badge></Button>
+        <Button className="mr-3">inline pill<Badge inline pill>24</Badge></Button>
         <Button outline color="link" radius="round" className="mr-3">link <Badge color="danger" alert>24</Badge></Button>
       </CardBody>
     </Card>
