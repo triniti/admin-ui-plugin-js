6d477f88386e2739d89928f906fafdcfbf5dc982
Matt Gollnick <mgollnick@gmail.com>
2018-02-07 10:12:45 -0800

    RE #58: Updated Datalist to use InputGroupAddon instead of InputGroupButton

--- src/components/datalist/index.jsx
+++ src/components/datalist/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, FormFeedback, Input, InputGroup, InputGroupButton } from '../../components';
+import { Button, FormFeedback, Input, InputGroup, InputGroupAddon } from '../../components';
 
 const Datalist = ({ addon, input, options, meta: { touched, error }, ...custom }) => (
   <div>
@@ -10,9 +10,9 @@ const Datalist = ({ addon, input, options, meta: { touched, error }, ...custom }
       {
         addon.show
         &&
-        <InputGroupButton>
+        <InputGroupAddon addonType="append">
           <Button outline color="light" onClick={addon.handleClick}><img src={addon.img} alt="button" /></Button>
-        </InputGroupButton>
+        </InputGroupAddon>
       }
       <datalist id={input.name}>
         {options.map((option, index) => <option value={option} key={`${option}-${index}`} />)}
