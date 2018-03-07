8ebe9155acb7c67fae052d8fb6886fc3f416fdd7
Matt Gollnick <mgollnick@gmail.com>
2018-03-06 19:27:54 -0800

    RE #75: - Added 6 new icons including book-open and status - Moved all custom variables into new file inside admin-ui apps folder - Defined 8 levels of drop shadow and replaced old instances - Card-header in sidebar no longer has drop shadow since sticky by default removed - Text-muted and text-light fine tuned - Badge vertically aligned in middle, tested badge with icons - Icon vertically aligned using margins - Styled img-thumbnail for possible use - Tons of fine tuning

--- demo/src/screens/badges/index.jsx
+++ demo/src/screens/badges/index.jsx
@@ -79,12 +79,12 @@ const UiBadge = () => (
 
       <Card key="badges">
         <CardBody indent>
-          <h1>H1 Heading .h1 <Badge>New</Badge></h1>
-          <h2>H2 Heading .h2 <Badge color="primary">New</Badge></h2>
-          <h3>H3 Heading .h3 <Badge color="success" pill>New</Badge></h3>
-          <h4>H4 Heading .h4 <Badge color="info">New</Badge></h4>
-          <h5>H5 Heading .h5 <Badge color="danger">New</Badge></h5>
-          <h6>h6 Heading .h6 <Badge pill>New</Badge></h6>
+          <h1>H1 Heading .h1 <Badge>New</Badge> <Badge><Icon imgSrc="warning-outline" className="mr-2" size="sd"/>New</Badge></h1>
+          <h2>H2 Heading .h2 <Badge color="primary">New</Badge> <Badge color="primary"><Icon imgSrc="warning-outline" className="mr-1"/>New</Badge></h2>
+          <h3>H3 Heading .h3 <Badge color="success" pill>New</Badge> <Badge color="success" pill><Icon imgSrc="warning-outline" className="mr-1"/>New</Badge></h3>
+          <h4>H4 Heading .h4 <Badge color="info">New</Badge> <Badge color="info"><Icon imgSrc="warning-outline" className="mr-1" size="sm"/>New</Badge></h4>
+          <h5>H5 Heading .h5 <Badge color="danger">New</Badge> <Badge color="danger"><Icon imgSrc="warning-outline" className="mr-1" size="xs"/>New</Badge></h5>
+          <h6>h6 Heading .h6 <Badge pill>New</Badge> <Badge pill><Icon imgSrc="warning-outline" className="mr-1" size="xxs"/>New</Badge></h6>
 
           <CardTitle tag="h4">Variations</CardTitle>
           <Badge>default</Badge>{' '}
@@ -96,6 +96,7 @@ const UiBadge = () => (
           <Badge color="danger">danger</Badge>{' '}
           <Badge color="dark">dark</Badge>{' '}
           <Badge color="light">light</Badge>{' '}
+          <Badge color="danger"><Icon imgSrc="warning-outline" size="xxs" className="mr-1"/>Danger</Badge>{' '}
 
           <CardTitle tag="h4">Pills</CardTitle>
           <Badge pill>default</Badge>{' '}
@@ -107,6 +108,7 @@ const UiBadge = () => (
           <Badge color="danger" pill>danger</Badge>{' '}
           <Badge color="dark" pill>dark</Badge>{' '}
           <Badge color="light" pill>light</Badge>{' '}
+          <Badge color="danger" pill><Icon imgSrc="warning-outline" size="xxs" className="mr-1"/>Danger</Badge>{' '}
 
           <CardTitle tag="h4">Badge Alert</CardTitle>
           <Button outline radius="round" className="mr-3">outline <Badge color="primary" alert>3</Badge></Button>
@@ -114,7 +116,7 @@ const UiBadge = () => (
           <Button className="mr-3">inline <Badge color="danger" className="ml-1">24</Badge></Button>
           <Button className="mr-3">inline pill <Badge pill className="ml-1">24</Badge></Button>
           <Button outline color="link" radius="round" className="mr-3">link <Badge color="danger" alert>24</Badge></Button>
-          <Button color="hover-bg" radius="circle" className="mr-3" size="md"><Icon imgSrc="user"/><Badge color="danger" alert>24</Badge></Button>
+          <Button color="hover-bg" radius="circle" className="mr-3" size="md"><Icon imgSrc="user"/><Badge color="danger" alert><Icon imgSrc="warning-outline" size="xxs"/></Badge></Button>
         </CardBody>
       </Card>,
     ]}
