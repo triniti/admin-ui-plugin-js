cfd39a04bd0714fc2299296c23475071cf8994df
Matt Gollnick <mgollnick@gmail.com>
2018-03-15 17:04:24 -0700

    RE #79: - Setup Tooltip UI Page for Paul - Added list-group-chat styles - Added avatar-initial styles to app/master - Added badge-indicator styles to badge - set z-index of card-header in sidebar to 2 so that scrolling content will go underneath without issues.  May need to increase.

--- demo/src/screens/tooltips/index.jsx
+++ demo/src/screens/tooltips/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PrimaryActions from '../../components/primary-actions';
 import Sidenav from '../../components/sidenav';
-import { Card, CardBody, Screen } from '../../../../src/components';
+import { Button, Card, CardBody, CardHeader, Collapse, Icon, Screen, Table, Tooltip } from '../../../../src/components';
 import TooltipItem from './TooltipItem';
 
 export default class TooltipsScreen extends React.Component {
   constructor(props) {
     super(props);
+
+    this.toggleTooltip = this.toggleTooltip.bind(this);
+    this.toggleCode1 = this.toggleCode1.bind(this);
 
     this.state = {
       tooltips: [
@@ -27,7 +30,19 @@ export default class TooltipsScreen extends React.Component {
           text: 'Right',
         },
       ],
+      tooltipOpen: false,
+      collapseCode1: false,
     };
+  }
+
+  toggleTooltip() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
+  }
+
+  toggleCode1() {
+    this.setState({ collapseCode1: !this.state.collapseCode1 });
   }
 
   render() {
@@ -37,15 +52,113 @@ export default class TooltipsScreen extends React.Component {
         sidenav={<Sidenav activeScreen="tooltips" />}
         sidenavHeader
         primaryActions={<PrimaryActions />}
-        body={
-          <Card>
+        body={[
+          <Card key="props">
+            <CardHeader>Button Group Properties</CardHeader>
+            <CardBody>
+              <Table hover responsive>
+                <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Type</th>
+                  <th>Default</th>
+                  <th>Description</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <th scope="row">'aria-label'</th>
+                  <td>string</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">className</th>
+                  <td>string</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">role</th>
+                  <td>string</td>
+                  <td>group</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">size</th>
+                  <td>string</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">tag</th>
+                  <td>string</td>
+                  <td>'div'</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">vertical</th>
+                  <td>bool</td>
+                  <td></td>
+                  <td>Stacks buttons in vertical alignment</td>
+                </tr>
+                </tbody>
+              </Table>
+            </CardBody>
+          </Card>,
+
+          <Card key="tooltips">
+            <CardHeader>Tooltip
+              <Button radius="circle" color="hover-bg" onClick={this.toggleCode1} active={this.state.collapseCode1}>
+                <Icon imgSrc="code" size="md"/>
+              </Button>
+            </CardHeader>
+            <Collapse isOpen={this.state.collapseCode1}>
+              <CardBody className="pl-5 bg-light">
+<pre>
+<code>{`import React from 'react';
+import { Tooltip } from '@triniti/admin-ui-plugin/components';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleTooltip = this.toggleTooltip.bind(this);
+    this.state = {
+      tooltipOpen: false
+    };
+  }
+
+  toggleTooltip() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
+  }
+
+  render() {
+    return ([
+        <a href="#" id="TooltipExample">Hover Me</a>,
+        <Tooltip placement="top" isOpen={this.state.tooltipOpen} target="TooltipExample" toggle={this.toggleTooltip}>
+          Tooltip Text
+        </Tooltip>,
+    ]);
+  }
+};`}</code></pre>
+              </CardBody>
+            </Collapse>
+            <CardBody indent>
+              <a href="#" id="TooltipExample">Hover Me to See Tooltip</a>
+              <Tooltip placement="top" isOpen={this.state.tooltipOpen} target="TooltipExample" toggle={this.toggleTooltip}>
+                Tooltip Text
+              </Tooltip>
+            </CardBody>
             <CardBody indent>
                 {this.state.tooltips.map((tooltip, i) => (
                   <TooltipItem key={tooltip.placement} item={tooltip} id={i} />
                 ))}
             </CardBody>
-          </Card>
-        }
+          </Card>,
+        ]}
       />
     );
   }
