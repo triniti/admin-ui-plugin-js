cb38e0a5a65d9997becf9641c14dde229660cebc
Matt Gollnick <mgollnick@gmail.com>
2018-03-19 14:14:17 -0700

    RE #79: - Added props for Tooltip and UncontrolledTooltip

--- demo/src/screens/tooltips/index.jsx
+++ demo/src/screens/tooltips/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PrimaryActions from '../../components/primary-actions';
 import Sidenav from '../../components/sidenav';
-import { Button, Card, CardBody, CardHeader, Collapse, Icon, Screen, Table, Tooltip } from '../../../../src/components';
+import { Button, Card, CardBody, CardHeader, Collapse, Icon, Screen, Table, Tooltip, UncontrolledTooltip } from '../../../../src/components';
 import TooltipItem from './TooltipItem';
 
 export default class TooltipsScreen extends React.Component {
@@ -10,6 +10,7 @@ export default class TooltipsScreen extends React.Component {
 
     this.toggleTooltip = this.toggleTooltip.bind(this);
     this.toggleCode1 = this.toggleCode1.bind(this);
+    this.toggleCode2 = this.toggleCode2.bind(this);
 
     this.state = {
       tooltips: [
@@ -32,6 +33,7 @@ export default class TooltipsScreen extends React.Component {
       ],
       tooltipOpen: false,
       collapseCode1: false,
+      collapseCode2: false,
     };
   }
 
@@ -45,6 +47,10 @@ export default class TooltipsScreen extends React.Component {
     this.setState({ collapseCode1: !this.state.collapseCode1 });
   }
 
+  toggleCode2() {
+    this.setState({ collapseCode2: !this.state.collapseCode2 });
+  }
+
   render() {
     return (
       <Screen
@@ -54,7 +60,7 @@ export default class TooltipsScreen extends React.Component {
         primaryActions={<PrimaryActions />}
         body={[
           <Card key="props">
-            <CardHeader>Button Group Properties</CardHeader>
+            <CardHeader>Tooltip Properties</CardHeader>
             <CardBody>
               <Table hover responsive>
                 <thead>
@@ -67,9 +73,9 @@ export default class TooltipsScreen extends React.Component {
                 </thead>
                 <tbody>
                 <tr>
-                  <th scope="row">'aria-label'</th>
-                  <td>string</td>
-                  <td></td>
+                  <th scope="row">autohide</th>
+                  <td>bool</td>
+                  <td>true</td>
                   <td></td>
                 </tr>
                 <tr>
@@ -79,28 +85,68 @@ export default class TooltipsScreen extends React.Component {
                   <td></td>
                 </tr>
                 <tr>
-                  <th scope="row">role</th>
-                  <td>string</td>
-                  <td>group</td>
+                  <th scope="row">container</th>
+                  <td>oneOfType</td>
                   <td></td>
+                  <td>
+                    Where to inject the popper DOM node, default to body<br/>
+                    PropTypes.string,<br/>
+                    PropTypes.func,<br/>
+                    DOMElement,
+                  </td>
                 </tr>
                 <tr>
-                  <th scope="row">size</th>
-                  <td>string</td>
-                  <td></td>
-                  <td></td>
+                  <th scope="row">delay</th>
+                  <td>oneOfType</td>
+                  <td>show: 0, hide: 250</td>
+                  <td>
+                    Optionally override show/hide delays <br/>
+                    {`PropTypes.shape({ show: PropTypes.number, hide: PropTypes.number })`},<br/>
+                    PropTypes.number
+                  </td>
                 </tr>
                 <tr>
-                  <th scope="row">tag</th>
-                  <td>string</td>
-                  <td>'div'</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <th scope="row">vertical</th>
+                  <th scope="row">disabled</th>
                   <td>bool</td>
                   <td></td>
-                  <td>Stacks buttons in vertical alignment</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">innerClassName</th>
+                  <td>string</td>
+                  <td></td>
+                  <td>Apply class to the inner-tooltip</td>
+                </tr>
+                <tr>
+                  <th scope="row">isOpen</th>
+                  <td>bool</td>
+                  <td>false</td>
+                  <td>Boolean to control the state of the tooltip</td>
+                </tr>
+                <tr>
+                  <th scope="row">placement</th>
+                  <td>oneOf(PopperPlacements)</td>
+                  <td>top</td>
+                  <td>'auto', 'auto-start', 'auto-end', 'top', 'top-start', 'top-end', 'right', 'right-start',
+                      'right-end', 'bottom', 'bottom-start', 'bottom-end', 'left', 'left-start', 'left-end'
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">placementPrefix</th>
+                  <td>string</td>
+                  <td nowrap>'bs-tooltip'</td>
+                  <td>Adds ability to style using a different class.</td>
+                </tr>
+                <tr>
+                  <th scope="row">target</th>
+                  <td>oneOfType</td>
+                  <td>*<i>Required</i></td>
+                  <td>
+                    Target element or element ID, popover is attached to this element<br/>
+                    PropTypes.string,<br/>
+                    PropTypes.func,<br/>
+                    DOMElement,
+                   </td>
                 </tr>
                 </tbody>
               </Table>
@@ -156,6 +202,36 @@ export default class Example extends React.Component {
                 {this.state.tooltips.map((tooltip, i) => (
                   <TooltipItem key={tooltip.placement} item={tooltip} id={i} />
                 ))}
+            </CardBody>
+          </Card>,
+
+          <Card key="uncontrolledtooltip">
+            <CardHeader>UncontrolledTooltip
+              <Button radius="circle" color="hover-bg" onClick={this.toggleCode2} active={this.state.collapseCode2}>
+                <Icon imgSrc="code" size="md"/>
+              </Button>
+            </CardHeader>
+            <Collapse isOpen={this.state.collapseCode2}>
+              <CardBody className="pl-5 bg-light">
+<pre>
+<code>{`import React from 'react';
+import { UncontrolledTooltip } from 'reactstrap';
+
+export default function Example() {
+    return (
+        <a href="#" id="UncontrolledTooltipExample">Hover Me to See UncontrolledTooltip</a>,
+        <UncontrolledTooltip placement="right" target="UncontrolledTooltipExample">
+          UncontrolledTooltip Text
+        </UncontrolledTooltip>,
+    );
+}`}</code></pre>
+              </CardBody>
+            </Collapse>
+            <CardBody indent>
+              <a href="#" id="UncontrolledTooltipExample">Hover Me to See UncontrolledTooltip</a>
+              <UncontrolledTooltip placement="right" target="UncontrolledTooltipExample">
+                UncontrolledTooltip Text
+              </UncontrolledTooltip>
             </CardBody>
           </Card>,
         ]}
