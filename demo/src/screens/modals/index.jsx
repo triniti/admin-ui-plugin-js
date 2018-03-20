9c1ea544760fba9e67a485d9e18c45eb0365c600
Matt Gollnick <mgollnick@gmail.com>
2018-03-20 10:15:38 -0700

    RE #79: - Added ModalBody, ModalHeader, ModalFooter props.  Added a few descriptions to Modal. - Moved example to top of page.

--- demo/src/screens/modals/index.jsx
+++ demo/src/screens/modals/index.jsx
@@ -67,156 +67,6 @@ class UiModal extends React.Component {
       // ]}
         primaryActions={<PrimaryActions />}
         body={[
-        <Card key="props">
-        <CardHeader>Modal Properties</CardHeader>
-        <CardBody>
-          <Table hover responsive>
-            <thead>
-            <tr>
-              <th>Name</th>
-              <th>Type</th>
-              <th>Default</th>
-              <th>Description</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <th scope="row">autoFocus</th>
-              <td>bool</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">backdrop</th>
-              <td>oneOfType</td>
-              <td>true</td>
-              <td>control backdrop<br/>PropTypes.bool<br/>PropTypes.oneOf(['static'])</td>
-            </tr>
-            <tr>
-              <th scope="row">backdropClassName</th>
-              <td>string</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">backdropTransition</th>
-              <td>string</td>
-              <td>150ms</td>
-              <td>Controls backdrop transition<br/>Timeout is 150ms by default to match bootstrap<br/>see Fade for more details</td>
-            </tr>
-            <tr>
-              <th scope="row">centered</th>
-              <td>bool</td>
-              <td></td>
-              <td>If modal should be centered vertically in viewport</td>
-            </tr>
-            <tr>
-              <th scope="row">className</th>
-              <td>string</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">cssModule</th>
-              <td>object</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">fade</th>
-              <td>bool</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">isOpen</th>
-              <td>bool</td>
-              <td></td>
-              <td>boolean to control the state of the popover</td>
-            </tr>
-            <tr>
-              <th scope="row">keyboard</th>
-              <td>bool</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">cssModule</th>
-              <td>object</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">labelledBy</th>
-              <td>string</td>
-              <td></td>
-              <td>Used to reference the ID of the title element in the modal</td>
-            </tr>
-            <tr>
-              <th scope="row">modalClassName</th>
-              <td>string</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">modalTransition</th>
-              <td>shape(Fade.propTypes)</td>
-              <td>300ms</td>
-              <td>modalTransition - controls modal transition<br/>timeout is 300ms by default to match bootstrap<br/>see Fade for more details</td>
-            </tr>
-            <tr>
-              <th scope="row">onClosed</th>
-              <td>func</td>
-              <td></td>
-              <td>Called when done transitioning out</td>
-            </tr>
-            <tr>
-              <th scope="row">onEnter</th>
-              <td>func</td>
-              <td></td>
-              <td>Allows for a node/componet to exist next to the modal (outside of it). Useful for external close buttons<br/>external: PropTypes.node,<br/>called on componentDidMount</td>
-            </tr>
-            <tr>
-              <th scope="row">onOpened</th>
-              <td>func</td>
-              <td></td>
-              <td>called when done transitioning in</td>
-            </tr>
-            <tr>
-              <th scope="row">role</th>
-              <td>string</td>
-              <td>dialog</td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">size</th>
-              <td>func</td>
-              <td></td>
-              <td>callback for toggling isOpen in the controlling component</td>
-            </tr>
-            <tr>
-              <th scope="row">toggle</th>
-              <td>bool</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">wrapClassName</th>
-              <td>string</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">zIndex</th>
-              <td>oneOfType</td>
-              <td>1000</td>
-              <td>PropTypes.number<br/>PropTypes.string
-              </td>
-            </tr>
-            </tbody>
-          </Table>
-        </CardBody>
-      </Card>,
           <Card key="modals">
             <CardHeader>Modals
               <Button radius="circle" color="hover-bg" onClick={this.toggleCode1} active={this.state.collapseCode1}>
@@ -319,7 +169,289 @@ export default ModalExample;
               </Modal>
             </CardBody>
           </Card>,
-      ]}
+
+          <Card key="props1">
+            <CardHeader>Modal Properties</CardHeader>
+            <CardBody>
+              <Table hover responsive>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Default</th>
+                    <th>Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">autoFocus</th>
+                    <td>bool</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">backdrop</th>
+                    <td>oneOfType</td>
+                    <td>true</td>
+                    <td>control backdrop<br/>PropTypes.bool,<br/>PropTypes.oneOf(['static']),<br/>
+                      see <a href="http://v4-alpha.getbootstrap.com/components/modal/#options" target="blank">
+                      http://v4-alpha.getbootstrap.com/components/modal/#options
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">backdropClassName</th>
+                    <td>string</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">backdropTransition</th>
+                    <td>string</td>
+                    <td>150ms</td>
+                    <td>Controls backdrop transition<br/>Timeout is 150ms by default to match bootstrap<br/>see Fade for more details</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">centered</th>
+                    <td>bool</td>
+                    <td></td>
+                    <td>If modal should be centered vertically in viewport</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">className</th>
+                    <td>string</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">fade</th>
+                    <td>bool</td>
+                    <td></td>
+                    <td>Boolean to control whether the fade transition occurs (default: true)</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">isOpen</th>
+                    <td>bool</td>
+                    <td></td>
+                    <td>Boolean to control the state of the popover</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">keyboard</th>
+                    <td>bool</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">cssModule</th>
+                    <td>object</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">labelledBy</th>
+                    <td>string</td>
+                    <td></td>
+                    <td>Used to reference the ID of the title element in the modal</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">maxWidth</th>
+                    <td>string</td>
+                    <td></td>
+                    <td>Allows you to set a custom max-width for the modal</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">modalClassName</th>
+                    <td>string</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">modalTransition</th>
+                    <td>shape(Fade.propTypes)</td>
+                    <td>300ms</td>
+                    <td>modalTransition - controls modal transition<br/>timeout is 300ms by default to match bootstrap<br/>see Fade for more details</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">onClosed</th>
+                    <td>func</td>
+                    <td></td>
+                    <td>Called when done transitioning out</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">onEnter</th>
+                    <td>func</td>
+                    <td></td>
+                    <td>Allows for a node/componet to exist next to the modal (outside of it). Useful for external close buttons<br/>external: PropTypes.node,<br/>called on componentDidMount</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">onExit</th>
+                    <td>func</td>
+                    <td></td>
+                    <td>Called on componentWillUnmount</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">onOpened</th>
+                    <td>func</td>
+                    <td></td>
+                    <td>Called when done transitioning in</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">role</th>
+                    <td>string</td>
+                    <td>dialog</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">size</th>
+                    <td>string</td>
+                    <td></td>
+                    <td>xs 240px, sm 300px, md 460px, lg 800px, xl 1000px</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">toggle</th>
+                    <td>func</td>
+                    <td></td>
+                    <td>Callback for toggling isOpen in the controlling component</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">wrapClassName</th>
+                    <td>string</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">zIndex</th>
+                    <td>oneOfType</td>
+                    <td>1050, set by bootstrap</td>
+                    <td>PropTypes.number,<br/>PropTypes.string,
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </CardBody>
+          </Card>,
+
+          <Card key="props2">
+            <CardHeader>ModalHeader Properties</CardHeader>
+            <CardBody>
+              <Table hover responsive>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Default</th>
+                    <th>Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">children</th>
+                    <td>node</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">className</th>
+                    <td>string</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">closeAriaLabel</th>
+                    <td>string</td>
+                    <td>'Close'</td>
+                    <td>Label added to close button.</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">tag</th>
+                    <td>oneOfType</td>
+                    <td>h4</td>
+                    <td>Tag wrapping header text.</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">toggle</th>
+                    <td>func</td>
+                    <td></td>
+                    <td>Toggles appearance of close button in header.</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">wrapTag</th>
+                    <td>oneOfType</td>
+                    <td>'div'</td>
+                    <td>Wrapping tag containing class modal-header.</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </CardBody>
+          </Card>,
+
+          <Card key="props3">
+            <CardHeader>ModalBody Properties</CardHeader>
+            <CardBody>
+              <Table hover responsive>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Default</th>
+                    <th>Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">className</th>
+                    <td>string</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">tag</th>
+                    <td>oneOfType</td>
+                    <td>div</td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </Table>
+            </CardBody>
+          </Card>,
+
+          <Card key="props4">
+            <CardHeader>ModalFooter Properties</CardHeader>
+            <CardBody>
+              <Table hover responsive>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Default</th>
+                    <th>Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">buttons</th>
+                    <td>bool</td>
+                    <td></td>
+                    <td>Adds class .modal-footer-buttons which makes buttons fill footer.</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">className</th>
+                    <td>string</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">tag</th>
+                    <td>oneOfType</td>
+                    <td>div</td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </Table>
+            </CardBody>
+          </Card>,
+
+        ]}
       />
     );
   }
