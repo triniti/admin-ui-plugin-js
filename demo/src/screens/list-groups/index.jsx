53351e4ee6ee6172fd9f839a80536fd660b3a1bd
Matt Gollnick <mgollnick@gmail.com>
2018-03-28 10:44:29 -0700

    RE #83: - Added example history list-group text for styling. - Removed maxWidth='768px' since History is regular width

--- demo/src/screens/list-groups/index.jsx
+++ demo/src/screens/list-groups/index.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 import PrimaryActions from '../../components/primary-actions';
 import Sidenav from '../../components/sidenav';
-import { Badge, Button, Card, CardBody, CardHeader, Icon, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText, Screen } from '../../../../src/components';
+import { Badge, Button, Card, CardBody, CardHeader, CardTitle, Collapse, Icon, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText, Screen } from '../../../../src/components';
 
-const UiListGroup = () => (
+class UiListGroup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggleEvent1 = this.toggleEvent1.bind(this);
+        this.toggleEvent2 = this.toggleEvent2.bind(this);
+        this.toggleEvent3 = this.toggleEvent3.bind(this);
+        this.toggleEvent4 = this.toggleEvent4.bind(this);
+        this.toggleEvent5 = this.toggleEvent5.bind(this);
+        this.state = {
+            collapseEvent1: false,
+            collapseEvent2: false,
+            collapseEvent3: false,
+            collapseEvent4: false,
+            collapseEvent5: false,
+        };
+    }
+
+    toggleEvent1() {
+        this.setState({ collapseEvent1: !this.state.collapseEvent1 });
+    }
+
+    toggleEvent2() {
+        this.setState({ collapseEvent2: !this.state.collapseEvent2 });
+    }
+
+    toggleEvent3() {
+        this.setState({ collapseEvent3: !this.state.collapseEvent3 });
+    }
+
+    toggleEvent4() {
+        this.setState({ collapseEvent4: !this.state.collapseEvent4 });
+    }
+
+    toggleEvent5() {
+        this.setState({ collapseEvent5: !this.state.collapseEvent5 });
+    }
+
+    render() {
+        return (
   <Screen
     sidenav={<Sidenav activeScreen="list-groups" />}
     sidenavHeader
@@ -239,9 +277,129 @@ const UiListGroup = () => (
           </ListGroup>
         </CardBody>
       </Card>,
+
+      <Card key="list-group11">
+        <CardHeader>List Group History - component Event Stream</CardHeader>
+        <CardBody>
+          <ListGroup className="list-group-history" borderless>
+
+            <ListGroupItem className="mb-0">
+              <ListGroupItemText>
+                <span className="mr-2 text-muted"><i>#1</i></span>
+                <span className="mr-2"><strong>Article Marked As Draft</strong></span>
+                by
+                <span className="ml-2"><strong><a href="">Joel Capillo</a></strong></span>
+                <span className="float-right"><i>an hour ago</i></span>
+              </ListGroupItemText>
+              <ListGroup borderless>
+                <ListGroupItem className="mb-0">
+                  <CardTitle tag="h5">
+                    <a className="card-title-sm text-info pr-2 pl-2" onClick={this.toggleEvent1} active={this.state.collapseEvent1}>
+                      View Event Details
+                    </a>
+                  </CardTitle>
+                  <Collapse isOpen={this.state.collapseEvent1}>
+                    <ul style={{border: '0px', padding: '0px', margin: '0.5em 0px 0.5em 0.125em', listStyle: 'none'}}><li style={{padding: '0px', margin: '0px'}}><ul style={{padding: '0px', margin: '0px', listStyle: 'none'}}><li style={{position: 'relative', paddingTop: '0.25em', marginLeft: '0px', paddingLeft: '0px'}}><div style={{display: 'inline-block', paddingRight: '0.5em', paddingLeft: '0px', cursor: 'pointer'}}><div style={{marginLeft: '0px', transition: '150ms', transform: 'rotateZ(0deg)', transformOrigin: '45% 50% 0px', position: 'relative', lineHeight: '1.1em', fontSize: '0.75em', color: 'rgb(99, 132, 160)'}}>▶</div></div><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px', padding: '0px', userSelect: 'inherit', cursor: 'pointer'}}><strong>Ctx User Ref</strong></label><span style={{paddingLeft: '0.5em', cursor: 'default', color: 'rgb(108, 116, 31)'}}><span><span style={{marginLeft: '0.3em', marginRight: '0.3em'}}>{}</span> 2 keys</span></span><ul style={{padding: '0px', margin: '0px', listStyle: 'none', display: 'block'}}></ul></li><li style={{position: 'relative', paddingTop: '0.25em', marginLeft: '0px', paddingLeft: '0px'}}><div style={{display: 'inline-block', paddingRight: '0.5em', paddingLeft: '0px', cursor: 'pointer'}}><div style={{marginLeft: '0px', transition: '150ms', transform: 'rotateZ(0deg)', transformOrigin: '45% 50% 0px', position: 'relative', lineHeight: '1.1em', fontSize: '0.75em', color: 'rgb(99, 132, 160)'}}>▶</div></div><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px', padding: '0px', userSelect: 'inherit', cursor: 'pointer'}}><strong>Ctx App</strong></label><span style={{paddingLeft: '0.5em', cursor: 'default', color: 'rgb(108, 116, 31)'}}><span><span style={{marginLeft: '0.3em', marginRight: '0.3em'}}>{}</span> 5 keys</span></span><ul style={{padding: '0px', margin: '0px', listStyle: 'none', display: 'block'}}></ul></li><li style={{paddingTop: '0.25em', paddingRight: '0px', marginLeft: '0.875em', userSelect: 'text', wordWrap: 'break-word', paddingLeft: '1.25em', textIndent: '-0.5em', wordBreak: 'break-all'}}><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px 0.5em 0px 0px'}}><strong>Ctx Ip</strong></label><span style={{color: 'rgb(108, 116, 31)'}}><strong class="bg-success text-white pl-2 pr-2">104.180.170.6</strong></span></li></ul></li></ul>
+                  </Collapse>
+              </ListGroupItem>
+              </ListGroup>
+            </ListGroupItem>
+
+            <ListGroupItem className="mb-0">
+              <ListGroupItemText>
+                <span className="mr-2 text-muted"><i>#2</i></span>
+                <span className="mr-2"><strong>Article Marked As Deleted</strong></span>
+                by
+                <span className="ml-2"><strong><a href="">Joel Capillo</a></strong></span>
+                <span className="float-right"><i>2 hours ago</i></span>
+              </ListGroupItemText>
+              <ListGroup borderless>
+                <ListGroupItem className="mb-0">
+                  <CardTitle tag="h5">
+                    <a className="card-title-sm text-info pr-2 pl-2" onClick={this.toggleEvent2} active={this.state.collapseEvent2}>
+                      View Event Details
+                    </a>
+                  </CardTitle>
+                  <Collapse isOpen={this.state.collapseEvent2}>
+                    <ul style={{border: '0px', padding: '0px', margin: '0.5em 0px 0.5em 0.125em', listStyle: 'none'}}><li style={{padding: '0px', margin: '0px'}}><ul style={{padding: '0px', margin: '0px', listStyle: 'none'}}><li style={{position: 'relative', paddingTop: '0.25em', marginLeft: '0px', paddingLeft: '0px'}}><div style={{display: 'inline-block', paddingRight: '0.5em', paddingLeft: '0px', cursor: 'pointer'}}><div style={{marginLeft: '0px', transition: '150ms', transform: 'rotateZ(0deg)', transformOrigin: '45% 50% 0px', position: 'relative', lineHeight: '1.1em', fontSize: '0.75em', color: 'rgb(99, 132, 160)'}}>▶</div></div><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px', padding: '0px', userSelect: 'inherit', cursor: 'pointer'}}><strong>Ctx User Ref</strong></label><span style={{paddingLeft: '0.5em', cursor: 'default', color: 'rgb(108, 116, 31)'}}><span><span style={{marginLeft: '0.3em', marginRight: '0.3em'}}>{}</span> 2 keys</span></span><ul style={{padding: '0px', margin: '0px', listStyle: 'none', display: 'block'}}></ul></li><li style={{position: 'relative', paddingTop: '0.25em', marginLeft: '0px', paddingLeft: '0px'}}><div style={{display: 'inline-block', paddingRight: '0.5em', paddingLeft: '0px', cursor: 'pointer'}}><div style={{marginLeft: '0px', transition: '150ms', transform: 'rotateZ(0deg)', transformOrigin: '45% 50% 0px', position: 'relative', lineHeight: '1.1em', fontSize: '0.75em', color: 'rgb(99, 132, 160)'}}>▶</div></div><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px', padding: '0px', userSelect: 'inherit', cursor: 'pointer'}}><strong>Ctx App</strong></label><span style={{paddingLeft: '0.5em', cursor: 'default', color: 'rgb(108, 116, 31)'}}><span><span style={{marginLeft: '0.3em', marginRight: '0.3em'}}>{}</span> 5 keys</span></span><ul style={{padding: '0px', margin: '0px', listStyle: 'none', display: 'block'}}></ul></li><li style={{paddingTop: '0.25em', paddingRight: '0px', marginLeft: '0.875em', userSelect: 'text', wordWrap: 'break-word', paddingLeft: '1.25em', textIndent: '-0.5em', wordBreak: 'break-all'}}><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px 0.5em 0px 0px'}}><strong>Ctx Ip</strong></label><span style={{color: 'rgb(108, 116, 31)'}}><strong class="bg-success text-white pl-2 pr-2">104.180.170.6</strong></span></li></ul></li></ul>
+                  </Collapse>
+              </ListGroupItem>
+              </ListGroup>
+            </ListGroupItem>
+
+            <ListGroupItem className="mb-0">
+              <ListGroupItemText>
+                <span className="mr-2 text-muted"><i>#3</i></span>
+                <span className="mr-2"><strong>Article Marked As Draft</strong></span>
+                by
+                <span className="ml-2"><strong><a href="">Joel Capillo</a></strong></span>
+                <span className="float-right"><i>15 hours ago</i></span>
+              </ListGroupItemText>
+              <ListGroup borderless>
+                <ListGroupItem className="mb-0">
+                  <CardTitle tag="h5">
+                    <a className="card-title-sm text-info pr-2 pl-2" onClick={this.toggleEvent3} active={this.state.collapseEvent3}>
+                      View Event Details
+                    </a>
+                  </CardTitle>
+                  <Collapse isOpen={this.state.collapseEvent3}>
+                    <ul style={{border: '0px', padding: '0px', margin: '0.5em 0px 0.5em 0.125em', listStyle: 'none'}}><li style={{padding: '0px', margin: '0px'}}><ul style={{padding: '0px', margin: '0px', listStyle: 'none'}}><li style={{position: 'relative', paddingTop: '0.25em', marginLeft: '0px', paddingLeft: '0px'}}><div style={{display: 'inline-block', paddingRight: '0.5em', paddingLeft: '0px', cursor: 'pointer'}}><div style={{marginLeft: '0px', transition: '150ms', transform: 'rotateZ(0deg)', transformOrigin: '45% 50% 0px', position: 'relative', lineHeight: '1.1em', fontSize: '0.75em', color: 'rgb(99, 132, 160)'}}>▶</div></div><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px', padding: '0px', userSelect: 'inherit', cursor: 'pointer'}}><strong>Ctx User Ref</strong></label><span style={{paddingLeft: '0.5em', cursor: 'default', color: 'rgb(108, 116, 31)'}}><span><span style={{marginLeft: '0.3em', marginRight: '0.3em'}}>{}</span> 2 keys</span></span><ul style={{padding: '0px', margin: '0px', listStyle: 'none', display: 'block'}}></ul></li><li style={{position: 'relative', paddingTop: '0.25em', marginLeft: '0px', paddingLeft: '0px'}}><div style={{display: 'inline-block', paddingRight: '0.5em', paddingLeft: '0px', cursor: 'pointer'}}><div style={{marginLeft: '0px', transition: '150ms', transform: 'rotateZ(0deg)', transformOrigin: '45% 50% 0px', position: 'relative', lineHeight: '1.1em', fontSize: '0.75em', color: 'rgb(99, 132, 160)'}}>▶</div></div><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px', padding: '0px', userSelect: 'inherit', cursor: 'pointer'}}><strong>Ctx App</strong></label><span style={{paddingLeft: '0.5em', cursor: 'default', color: 'rgb(108, 116, 31)'}}><span><span style={{marginLeft: '0.3em', marginRight: '0.3em'}}>{}</span> 5 keys</span></span><ul style={{padding: '0px', margin: '0px', listStyle: 'none', display: 'block'}}></ul></li></ul></li></ul>
+                  </Collapse>
+              </ListGroupItem>
+              </ListGroup>
+            </ListGroupItem>
+
+            <ListGroupItem className="mb-0">
+              <ListGroupItemText>
+                <span className="mr-2 text-muted"><i>#4</i></span>
+                <span className="mr-2"><strong>Article Marked As Deleted</strong></span>
+                by
+                <span className="ml-2"><strong><a href="">Joel Capillo</a></strong></span>
+                <span className="float-right"><i>17 hours ago</i></span>
+              </ListGroupItemText>
+              <ListGroup borderless>
+                <ListGroupItem className="mb-0">
+                  <CardTitle tag="h5">
+                    <a className="card-title-sm text-info pr-2 pl-2" onClick={this.toggleEvent4} active={this.state.collapseEvent4}>
+                      View Event Details
+                    </a>
+                  </CardTitle>
+                  <Collapse isOpen={this.state.collapseEvent4}>
+                    <ul style={{border: '0px', padding: '0px', margin: '0.5em 0px 0.5em 0.125em', listStyle: 'none'}}><li style={{padding: '0px', margin: '0px'}}><ul style={{padding: '0px', margin: '0px', listStyle: 'none'}}><li style={{position: 'relative', paddingTop: '0.25em', marginLeft: '0px', paddingLeft: '0px'}}><div style={{display: 'inline-block', paddingRight: '0.5em', paddingLeft: '0px', cursor: 'pointer'}}><div style={{marginLeft: '0px', transition: '150ms', transform: 'rotateZ(0deg)', transformOrigin: '45% 50% 0px', position: 'relative', lineHeight: '1.1em', fontSize: '0.75em', color: 'rgb(99, 132, 160)'}}>▶</div></div><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px', padding: '0px', userSelect: 'inherit', cursor: 'pointer'}}><strong>Ctx User Ref</strong></label><span style={{paddingLeft: '0.5em', cursor: 'default', color: 'rgb(108, 116, 31)'}}><span><span style={{marginLeft: '0.3em', marginRight: '0.3em'}}>{}</span> 2 keys</span></span><ul style={{padding: '0px', margin: '0px', listStyle: 'none', display: 'block'}}></ul></li><li style={{position: 'relative', paddingTop: '0.25em', marginLeft: '0px', paddingLeft: '0px'}}><div style={{display: 'inline-block', paddingRight: '0.5em', paddingLeft: '0px', cursor: 'pointer'}}><div style={{marginLeft: '0px', transition: '150ms', transform: 'rotateZ(0deg)', transformOrigin: '45% 50% 0px', position: 'relative', lineHeight: '1.1em', fontSize: '0.75em', color: 'rgb(99, 132, 160)'}}>▶</div></div><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px', padding: '0px', userSelect: 'inherit', cursor: 'pointer'}}><strong>Ctx App</strong></label><span style={{paddingLeft: '0.5em', cursor: 'default', color: 'rgb(108, 116, 31)'}}><span><span style={{marginLeft: '0.3em', marginRight: '0.3em'}}>{}</span> 5 keys</span></span><ul style={{padding: '0px', margin: '0px', listStyle: 'none', display: 'block'}}></ul></li></ul></li></ul>
+                  </Collapse>
+              </ListGroupItem>
+              </ListGroup>
+            </ListGroupItem>
+
+            <ListGroupItem className="mb-0">
+              <ListGroupItemText>
+                <span className="mr-2 text-muted"><i>#5</i></span>
+                <span className="mr-2"><strong>Article Marked As Updated</strong></span>
+                by
+                <span className="ml-2"><strong><a href="">Joel Capillo</a></strong></span>
+                <span className="float-right"><i>5 days ago</i></span>
+              </ListGroupItemText>
+              <ListGroup borderless>
+                <ListGroupItem className="mb-0">
+                  <CardTitle tag="h5">
+                    <a className="card-title-sm text-info pr-2 pl-2" onClick={this.toggleEvent5} active={this.state.collapseEvent5}>
+                      View Event Details
+                    </a>
+                  </CardTitle>
+                  <Collapse isOpen={this.state.collapseEvent5}>
+                    <ul style={{border: '0px', padding: '0px', margin: '0.5em 0px 0.5em 0.125em', listStyle: 'none'}}><li style={{padding: '0px', margin: '0px'}}><ul style={{padding: '0px', margin: '0px', listStyle: 'none'}}><li style={{position: 'relative', paddingTop: '0.25em', marginLeft: '0px', paddingLeft: '0px'}}><div style={{display: 'inline-block', paddingRight: '0.5em', paddingLeft: '0px', cursor: 'pointer'}}><div style={{marginLeft: '0px', transition: '150ms', transform: 'rotateZ(0deg)', transformOrigin: '45% 50% 0px', position: 'relative', lineHeight: '1.1em', fontSize: '0.75em', color: 'rgb(99, 132, 160)'}}>▶</div></div><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px', padding: '0px', userSelect: 'inherit', cursor: 'pointer'}}><strong>Ctx User Ref</strong></label><span style={{paddingLeft: '0.5em', cursor: 'default', color: 'rgb(108, 116, 31)'}}><span><span style={{marginLeft: '0.3em', marginRight: '0.3em'}}>{}</span> 2 keys</span></span><ul style={{padding: '0px', margin: '0px', listStyle: 'none', display: 'block'}}></ul></li><li style={{position: 'relative', paddingTop: '0.25em', marginLeft: '0px', paddingLeft: '0px'}}><div style={{display: 'inline-block', paddingRight: '0.5em', paddingLeft: '0px', cursor: 'pointer'}}><div style={{marginLeft: '0px', transition: '150ms', transform: 'rotateZ(0deg)', transformOrigin: '45% 50% 0px', position: 'relative', lineHeight: '1.1em', fontSize: '0.75em', color: 'rgb(99, 132, 160)'}}>▶</div></div><label style={{display: 'inline-block', color: 'rgb(99, 132, 160)', margin: '0px', padding: '0px', userSelect: 'inherit', cursor: 'pointer'}}><strong>Ctx App</strong></label><span style={{paddingLeft: '0.5em', cursor: 'default', color: 'rgb(108, 116, 31)'}}><span><span style={{marginLeft: '0.3em', marginRight: '0.3em'}}>{}</span> 5 keys</span></span><ul style={{padding: '0px', margin: '0px', listStyle: 'none', display: 'block'}}></ul></li></ul></li></ul>
+                  </Collapse>
+              </ListGroupItem>
+              </ListGroup>
+            </ListGroupItem>
+
+          </ListGroup>
+        </CardBody>
+      </Card>,
     ]}
-    maxWidth='768px'
-  />
-);
+    />
+        );
+    }
+}
 
 export default UiListGroup;
