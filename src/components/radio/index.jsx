6fff9dd1707be83b0ea929b1ac272335e064974d
Matt Gollnick <mgollnick@gmail.com>
2018-03-29 16:41:22 -0700

    RE #83: - Updated Checkbox component to allow for children as label, no longer need IDs, input now fills label area for cursor state - Updated Radio component to allow for children as label, no longer need IDs, input now fills label area for cursor state - Cleaned up form css, fixing form-check-inline margins and simplifying disabled state - Added ability to pass classes to label in Switch component. Still need to update like I did for Checkbox and Radio - Updated all Radio, Checkbox and Switch examples in forms and buttons UI pages.

--- src/components/radio/index.jsx
+++ src/components/radio/index.jsx
@@ -7,37 +7,50 @@ import Label from '../label';
 import './styles.scss';
 
 const propTypes = {
+  children: PropTypes.node,
   className: PropTypes.string,
   cssModule: PropTypes.object,
+  disabled: PropTypes.bool,
+  right: PropTypes.bool,
+  size: PropTypes.string,
   type: PropTypes.string,
-  id: PropTypes.string,
-  name: PropTypes.string,
 };
 
 const defaultProps = {
   type: 'radio',
-  id: 'radio',
-  name: 'radio',
 };
 
 const Radio = (props) => {
   const {
+    children,
     className,
     cssModule,
+    disabled,
+    right,
+    size,
     type,
-    id,
-    name,
     ...attributes
   } = props;
   const classes = mapToCssModules(classNames(
     className,
     'radio-input',
+    size ? `form-control form-control-${size}` : 'form-control',
   ), cssModule);
 
-  return [
-    <Input {...attributes} type={type} id={id} className={classes} key={`${name}-radio-input`} />,
-    <Label for={id} className="radio-input-label" key={`${name}-radio-label`} />,
-  ];
+  const labelClasses = mapToCssModules(classNames(
+    className,
+    'radio-input-label',
+    disabled ? 'radio-disabled' : false,
+    right ? 'radio-label-right' : false,
+  ), cssModule);
+
+  return (
+    <Label className={labelClasses} radio>
+      <Input type={type} className={classes} {...attributes} disabled={disabled} />
+      <span className="radio-input-icon"/>
+      {children && <span className="form-radio-label radio-label">{children}</span>}
+    </Label>
+  );
 };
 
 Radio.propTypes = propTypes;
