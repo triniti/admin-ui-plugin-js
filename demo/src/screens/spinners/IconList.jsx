6ca1dbe7ad109f3474ae648b1ac979d3bd88853a
paul.marion <paul.marion@wbdevops.com>
2018-04-02 17:35:06 -0700

    Re #173 copying icons folder and created a new one for spinners, took out spinners in icons for their own new one

---
+++ demo/src/screens/spinners/IconList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Icon } from '../../../../src/components';
+import './styles.scss';
+
+
+const IconList = ({ iconName, ...attributes }) => ([
+    <div className="iconlist-item-wrapper" key={`list-${iconName}`}>
+        <Icon imgSrc={iconName} {...attributes} size="lg" />
+        <div className="iconlist-item-text">{iconName}</div>
+    </div>
+]);
+
+IconList.propTypes = {
+  iconName: PropTypes.string,
+};
+
+export default IconList;
