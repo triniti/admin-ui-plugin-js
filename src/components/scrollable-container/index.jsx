72f529bdcf0aceb411d6078cb789126cc82f7672
Matt Gollnick <mgollnick@gmail.com>
2018-04-16 10:29:38 -0700

    RE #88: - Add ability to pass className and attributes to ScrollableContainer component - Add smooth scrolling to modal on mobile

--- src/components/scrollable-container/index.jsx
+++ src/components/scrollable-container/index.jsx
@@ -1,15 +1,40 @@
-// react
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { mapToCssModules } from '../utils';
 import './styles.scss';
 
-
-const ScrollableContainer = ({ children }) => (
-  <div className="scrollable-container">{children}</div>
-);
-
-ScrollableContainer.propTypes = {
+const propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
+  cssModule: PropTypes.object,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
 };
+
+const defaultProps = {
+  tag: 'div',
+};
+
+const ScrollableContainer = (props) => {
+  const {
+    children,
+    className,
+    cssModule,
+    tag: Tag,
+    ...attributes
+  } = props;
+
+  const classes = mapToCssModules(classNames(
+    className,
+    'scrollable-container',
+  ), cssModule);
+
+  return (
+    <Tag {...attributes} className={classes}>{children}</Tag>
+  );
+};
+
+ScrollableContainer.propTypes = propTypes;
+ScrollableContainer.defaultProps = defaultProps;
 
 export default ScrollableContainer;
