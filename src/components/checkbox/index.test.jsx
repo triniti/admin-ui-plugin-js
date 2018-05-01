3f09b60efaa00e8217168f62dc2fb6d3afc8e1a9
weiwang314 <wei.wang@wbdevops.com>
2018-05-01 15:50:37 -0700

    checkbox tests

--- src/components/checkbox/index.test.jsx
+++ src/components/checkbox/index.test.jsx
@@ -16,3 +16,15 @@ test('Checkbox:: should render default props', (t) => {
 
   t.end();
 });
+
+test('Checkbox:: should render custom props', (t) => {
+  const wrapper = shallow(<Checkbox size="lg" disabled right round />);
+
+  t.equal(wrapper.find('.checkbox-label-right').length, 1);
+  t.equal(wrapper.find('.form-control-lg').length, 1);
+  t.equal(wrapper.find('.checkbox-disabled').length, 1);
+  t.equal(wrapper.find('.checkbox-input-round').length, 1);
+
+  t.end();
+});
+
