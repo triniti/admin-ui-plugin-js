91c9be7d66c00e206f460d1acee6133845df00b2
weiwang314 <wei.wang@wbdevops.com>
2018-05-04 15:04:37 -0700

    add async select example

--- demo/src/screens/select/inputselect.jsx
+++ demo/src/screens/select/inputselect.jsx
@@ -1,66 +1,40 @@
 import React from 'react';
-import createClass from 'create-react-class';
-import PropTypes from 'prop-types';
 import { Select } from '../../../../src/components';
 
-const FLAVOURS = [
-	{ label: 'Triniti', value: 'trinit' },
-	{ label: 'Raven', value: 'raven' },
-	{ label: 'EME', value: 'eme' },
-	{ label: 'BOTS', value: 'bots' },
-	{ label: 'Crowd Fusion', value: 'crowdfusion' },
-	{ label: 'React', value: 'react' },
+const OPTIONS = [
+  { label: 'Triniti', value: 'trinit' },
+  { label: 'Raven', value: 'raven' },
+  { label: 'EME', value: 'eme' },
+  { label: 'BOTS', value: 'bots' },
+  { label: 'Crowd Fusion', value: 'crowdfusion' },
+  { label: 'React', value: 'react' },
 ];
 
-var InputSelectField = createClass({
-	displayName: 'InputSelectField',
-	propTypes: {
-		label: PropTypes.string,
-	},
-	getInitialState () {
-		return {
-			removeSelected: true,
-			disabled: false,
-			crazy: false,
-			stayOpen: false,
-			value: [],
-			rtl: false,
-		};
-	},
-	handleSelectChange (value) {
-		console.log('You\'ve selected:', value);
-		this.setState({ value });
-	},
-	toggleCheckbox (e) {
-		this.setState({
-			[e.target.name]: e.target.checked,
-		});
-	},
-	toggleRtl (e) {
-		let rtl = e.target.checked;
-		this.setState({ rtl });
-	},
+export default class InputSelectField extends React.Component {
+  constructor(props) {
+    super(props);
 
-	render () {
-		const { disabled, stayOpen, value } = this.state;
-		const options = FLAVOURS;
-		return (
-			<Select
-				closeOnSelect={!stayOpen}
-				disabled={disabled}
-				multi
-				onChange={this.handleSelectChange}
-				options={options}
-				placeholder="Select your favourite(s)"
-	  removeSelected={this.state.removeSelected}
-				rtl={this.state.rtl}
-				simpleValue
-				value={value}
-				size="lg"
-			/>
-		);
-	}
-});
+    this.state = { value: [] };
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
 
+  handleSelectChange(value) {
+    console.log('You\'ve selected:', value);
+    this.setState({ value });
+  }
 
-export default InputSelectField;
+  render() {
+    const { value } = this.state;
+    return (
+      <Select
+        multi
+        onChange={this.handleSelectChange}
+        options={OPTIONS}
+        placeholder="Select your option(s)"
+        simpleValue
+        value={value}
+        size="lg"
+      />
+    );
+  }
+}
--- demo/src/screens/select/multiselect.jsx
+++ demo/src/screens/select/multiselect.jsx
@@ -1,91 +1,91 @@
 import React from 'react';
-import createClass from 'create-react-class';
-import PropTypes from 'prop-types';
 import { FormGroup, Checkbox, Select } from '../../../../src/components';
 
 const FLAVOURS = [
-	{ label: 'Chocolate', value: 'chocolate' },
-	{ label: 'Vanilla', value: 'vanilla' },
-	{ label: 'Strawberry', value: 'strawberry' },
-	{ label: 'Caramel', value: 'caramel' },
-	{ label: 'Cookies and Cream', value: 'cookiescream' },
-	{ label: 'Peppermint', value: 'peppermint' },
+  { label: 'Chocolate', value: 'chocolate' },
+  { label: 'Vanilla', value: 'vanilla' },
+  { label: 'Strawberry', value: 'strawberry' },
+  { label: 'Caramel', value: 'caramel' },
+  { label: 'Cookies and Cream', value: 'cookiescream' },
+  { label: 'Peppermint', value: 'peppermint' },
 ];
 
 const WHY_WOULD_YOU = [
-	{ label: 'Chocolate (are you crazy?)', value: 'chocolate', disabled: true },
+  { label: 'Chocolate (are you crazy?)', value: 'chocolate', disabled: true },
 ].concat(FLAVOURS.slice(1));
 
-var MultiSelectField = createClass({
-	displayName: 'MultiSelectField',
-	propTypes: {
-		label: PropTypes.string,
-	},
-	getInitialState () {
-		return {
-			removeSelected: true,
-			disabled: false,
-			crazy: false,
-			stayOpen: false,
-			value: [],
-			rtl: false,
-		};
-	},
-	handleSelectChange (value) {
-		console.log('You\'ve selected:', value);
-		this.setState({ value });
-	},
-	toggleCheckbox (e) {
-		this.setState({
-			[e.target.name]: e.target.checked,
-		});
-	},
-	toggleRtl (e) {
-		let rtl = e.target.checked;
-		this.setState({ rtl });
-	},
+export default class MultiSelectField extends React.Component {
+  constructor(props) {
+    super(props);
 
-	render () {
-		const { crazy, disabled, stayOpen, value } = this.state;
-		const options = crazy ? WHY_WOULD_YOU : FLAVOURS;
-		return (
-			<div className="section">
-			  <FormGroup>
-				<Select
-					closeOnSelect={!stayOpen}
-					disabled={disabled}
-					multi
-					onChange={this.handleSelectChange}
-					options={options}
-					placeholder="Select your favourite(s)"
-          removeSelected={this.state.removeSelected}
-					rtl={this.state.rtl}
-					simpleValue
-					value={value}
-				/>
-              </FormGroup>
+    this.state = {
+      removeSelected: true,
+      disabled: false,
+      crazy: false,
+      stayOpen: false,
+      value: [],
+      rtl: false,
+    };
 
-				<FormGroup>
-				  <FormGroup check>
-                    <Checkbox size="sm" name="removeSelected" checked={this.state.removeSelected} onChange={this.toggleCheckbox}>Remove selected options</Checkbox>
-                  </FormGroup>
-                  <FormGroup check>
-                    <Checkbox size="sm" name="disabled" checked={this.state.disabled} onChange={this.toggleCheckbox}>Disable the control</Checkbox>
-                  </FormGroup>
-                  <FormGroup check>
-                    <Checkbox size="sm" name="crazy" checked={crazy} onChange={this.toggleCheckbox}>I don't like Chocolate (disabled the option)</Checkbox>
-                  </FormGroup>
-                  <FormGroup check>
-                    <Checkbox size="sm" name="stayOpen" checked={stayOpen} onChange={this.toggleCheckbox}>Stay open when an Option is selected</Checkbox>
-                  </FormGroup>
-                  <FormGroup check>
-                    <Checkbox size="sm" name="rtl" checked={this.state.rtl} onChange={this.toggleCheckbox}>rtl</Checkbox>
-                  </FormGroup>
-				</FormGroup>
-			</div>
-		);
-	}
-});
-module.exports = MultiSelectField;
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.toggleCheckbox = this.toggleCheckbox.bind(this);
+    this.toggleRtl = this.toggleRtl.bind(this);
+  }
 
-export default MultiSelectField;
+  handleSelectChange(value) {
+    console.log('You\'ve selected:', value);
+    this.setState({ value });
+  }
+
+  toggleCheckbox(e) {
+    this.setState({
+      [e.target.name]: e.target.checked,
+    });
+  }
+
+  toggleRtl(e) {
+    const rtl = e.target.checked;
+    this.setState({ rtl });
+  }
+
+  render() {
+    const { crazy, disabled, stayOpen, value } = this.state;
+    const options = crazy ? WHY_WOULD_YOU : FLAVOURS;
+    return (
+      <div className="section">
+        <FormGroup>
+          <Select
+            closeOnSelect={!stayOpen}
+            disabled={disabled}
+            multi
+            onChange={this.handleSelectChange}
+            options={options}
+            placeholder="Select your favourite(s)"
+            removeSelected={this.state.removeSelected}
+            rtl={this.state.rtl}
+            simpleValue
+            value={value}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <FormGroup check>
+            <Checkbox size="sm" name="removeSelected" checked={this.state.removeSelected} onChange={this.toggleCheckbox}>Remove selected options</Checkbox>
+          </FormGroup>
+          <FormGroup check>
+            <Checkbox size="sm" name="disabled" checked={this.state.disabled} onChange={this.toggleCheckbox}>Disable the control</Checkbox>
+          </FormGroup>
+          <FormGroup check>
+            <Checkbox size="sm" name="crazy" checked={crazy} onChange={this.toggleCheckbox}>I don't like Chocolate (disabled the option)</Checkbox>
+          </FormGroup>
+          <FormGroup check>
+            <Checkbox size="sm" name="stayOpen" checked={stayOpen} onChange={this.toggleCheckbox}>Stay open when an Option is selected</Checkbox>
+          </FormGroup>
+          <FormGroup check>
+            <Checkbox size="sm" name="rtl" checked={this.state.rtl} onChange={this.toggleCheckbox}>rtl</Checkbox>
+          </FormGroup>
+        </FormGroup>
+      </div>
+    );
+  }
+}
