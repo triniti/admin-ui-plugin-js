576065258be8458591712abfcd9bc92dde9eb249
Matt Gollnick <mgollnick@gmail.com>
2018-05-24 16:12:55 -0700

    RE #100: - Added bg-none class to remove backgrounds - Fixed test for Media component - Updated Media prop from hover bool to hover string - Kept Media prop hoverOutline but it is deprecated - Added sample code for Spinner component

--- src/components/media/index.jsx
+++ src/components/media/index.jsx
@@ -4,11 +4,15 @@ import classNames from 'classnames';
 import { Media as MediaRS } from 'reactstrap';
 import './styles.scss';
 
-const Media = ({ aspectRatio, className, hover, ...attributes }) => {
+const Media = ({ aspectRatio, className, hover, hoverOutline, ...attributes }) => {
   const classes = classNames(
     className,
+    {
+      'aspect-ratio': aspectRatio,
+      'media-hover-outline': hoverOutline,
+    },
     { [`media-hover-${hover}`]: !!hover },
-    { [`aspect-ratio aspect-ratio-${aspectRatio}`]: !!aspectRatio },
+    { [`aspect-ratio-${aspectRatio}`]: !!aspectRatio },
   );
 
   return <MediaRS {...attributes} className={classes} />;
@@ -18,12 +22,14 @@ Media.propTypes = {
   aspectRatio: PropTypes.string,
   className: PropTypes.string,
   hover: PropTypes.string,
+  hoverOutline: PropTypes.bool, /* deprecated use hover="outline" */
 };
 
 Media.defaultProps = {
   aspectRatio: '',
   className: '',
   hover: '',
+  hoverOutline: false,
 };
 
 export default Media;
