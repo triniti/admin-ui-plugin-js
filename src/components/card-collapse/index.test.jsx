e66b77c1a2d3b50bd6846d9fbafff4372653fdf5
weiwang314 <wei.wang@wbdevops.com>
2018-07-02 15:07:37 -0700

    add one test

--- src/components/card-collapse/index.test.jsx
+++ src/components/card-collapse/index.test.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import test from 'tape';
 import { shallow } from 'enzyme';
-import { Collapse as CardCollapseRS } from 'reactstrap';
+import { Collapse } from 'reactstrap';
+import Button from '../button';
 // eslint-disable-next-line
 import CardCollapse from './';
 
 test('CardCollapse:: should import CardCollapse component from reactstrap', (t) => {
   const wrapper = shallow(<CardCollapse />);
 
-  t.equal(wrapper.find(CardCollapseRS).length, 1);
+  t.equal(wrapper.find(Collapse).length, 1);
   t.equal(wrapper.find('.card-collapse').length, 1);
   t.end();
 });
 
 test('CardCollapse:: should use custom props to modify class names', (t) => {
-  const wrapper = shallow(<CardCollapse isOpen isStacked className="additional-class" />);
+  const wrapper = shallow(<CardCollapse title="hello" isOpen isStacked className="additional-class" />);
 
   t.equal(wrapper.find('.mb-0').length, 1);
   t.equal(wrapper.find('.additional-class').length, 1);
+  t.equal(wrapper.find(Button).children().text(), 'hello');
   t.true(wrapper.html().includes('collapse show'));
   t.true(wrapper.html().includes('btn btn-toggler active'));
 
