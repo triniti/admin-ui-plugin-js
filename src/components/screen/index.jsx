dcf52b69e47fa83d1af2024a6c2006116ef1a5af
weiwang314 <wei.wang@wbdevops.com>
2018-07-20 15:51:52 -0700

    update screen for default tab

--- src/components/screen/index.jsx
+++ src/components/screen/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
 import { BreadcrumbItem, NavItem } from 'reactstrap';
 
 import AlertBar from '../alert-bar';
@@ -39,19 +40,20 @@ class Screen extends React.Component {
 
   render() {
     const {
-      dispatch,
       alerts,
-      header,
-      sidenav,
-      sidenavHeader,
-      sidebar,
-      tabs,
       body,
+      breadcrumbs,
+      dispatch,
       footer,
+      header,
+      match,
+      maxWidth,
       primaryActions,
       secondaryActions,
-      breadcrumbs,
-      maxWidth,
+      sidebar,
+      sidenav,
+      sidenavHeader,
+      tabs,
     } = this.props;
 
     const { isSidebarOpen, isSidenavOpen } = this.state;
@@ -60,66 +62,89 @@ class Screen extends React.Component {
 
     return (
       <div className="screen">
-        {sidenav &&
-        <div className={sidenavClassess} id="sidenav">
-          {sidenavHeader !== null &&
-          <div className="screen-sidenav-header">
-            {sidenavHeader && <h3 className="screen-sidenav-header-title">{sidenavHeader}</h3>}
-            <Button outline color="hover" className="screen-sidenav-toggler" onClick={this.toggleSidenav}>
-              <Icon imgSrc="arrow-left" alt="close" className="screen-sidenav-toggler-img" />
-            </Button>
-          </div>}
-          <div className="screen-sidenav-body">{sidenav}</div>
-        </div>}
+        {sidenav
+        && (
+          <div className={sidenavClassess} id="sidenav">
+            {sidenavHeader !== null
+            && (
+              <div className="screen-sidenav-header">
+                {sidenavHeader && <h3 className="screen-sidenav-header-title">{sidenavHeader}</h3>}
+                <Button outline color="hover" className="screen-sidenav-toggler" onClick={this.toggleSidenav}>
+                  <Icon imgSrc="arrow-left" alt="close" className="screen-sidenav-toggler-img" />
+                </Button>
+              </div>
+            )}
+            <div className="screen-sidenav-body">{sidenav}</div>
+          </div>
+        )}
 
         <div className="screen-main">
-          {(header || primaryActions || breadcrumbs.length) &&
-          <div className="screen-header-container">
-            {breadcrumbs.length > 0 &&
-            <h1 className="screen-header-title">
-              <Breadcrumb>
-                {breadcrumbs.map(breadcrumb => (
-                  <BreadcrumbItem key={breadcrumb.text}>
-                    {breadcrumb.to && <RouterLink to={breadcrumb.to}>{breadcrumb.text}</RouterLink>}
-                    {!breadcrumb.to && breadcrumb.text}
-                  </BreadcrumbItem>
-                ))}
-              </Breadcrumb>
-            </h1>}
-            {breadcrumbs.length === 0 && header && <h1 className="screen-header-title">{header}</h1>}
-            {primaryActions && <ScreenPrimaryActions>{primaryActions}</ScreenPrimaryActions>}
-          </div>}
+          {(header || primaryActions || breadcrumbs.length)
+          && (
+            <div className="screen-header-container">
+              {breadcrumbs.length > 0
+              && (
+                <h1 className="screen-header-title">
+                  <Breadcrumb>
+                    {breadcrumbs.map(breadcrumb => (
+                      <BreadcrumbItem key={breadcrumb.text}>
+                        {breadcrumb.to && <RouterLink to={breadcrumb.to}>{breadcrumb.text}</RouterLink>}
+                        {!breadcrumb.to && breadcrumb.text}
+                      </BreadcrumbItem>
+                    ))}
+                  </Breadcrumb>
+                </h1>
+              )}
+              {breadcrumbs.length === 0 && header && <h1 className="screen-header-title">{header}</h1>}
+              {primaryActions && <ScreenPrimaryActions>{primaryActions}</ScreenPrimaryActions>}
+            </div>
+          )}
 
           <AlertBar alerts={alerts} onDismiss={id => dispatch(dismissAlert(id))} />
 
-          {tabs.length > 0 &&
-          <Nav underline className="screen-navtabs">
-            {tabs.map(tab => (
-              <NavItem key={tab.to}>
-                <RouterLink navTab to={tab.to}>{tab.text}</RouterLink>
-              </NavItem>
-            ))}
-          </Nav>}
+          {tabs.length > 0
+          && (
+            <Nav underline className="screen-navtabs">
+              {tabs.map((tab, i) => {
+                let className = '';
+                if (i === 0 && !get(match, 'params.tab')) {
+                  className = 'active';
+                }
+
+                return (
+                  <NavItem key={tab.to}>
+                    <RouterLink navTab to={tab.to} className={className}>
+                      {tab.text}
+                    </RouterLink>
+                  </NavItem>
+                );
+              })}
+            </Nav>
+          )}
 
           <div className="screen-body-container">
             <div className="screen-body">
               <div className="screen-body-content" style={{ maxWidth }}>{body}</div>
             </div>
-            {sidebar &&
-            <div className={sidebarClassess} id="screen-sidebar">
-              <Button radius="circle" color="info" className="screen-sidebar-toggler" onClick={this.toggleSidebar}>
-                <Icon imgSrc="arrow-left-thick" alt="arrow" />
-              </Button>
-              <div className="screen-sidebar-body">{sidebar}</div>
-            </div>}
+            {sidebar
+            && (
+              <div className={sidebarClassess} id="screen-sidebar">
+                <Button radius="circle" color="info" className="screen-sidebar-toggler" onClick={this.toggleSidebar}>
+                  <Icon imgSrc="arrow-left-thick" alt="arrow" />
+                </Button>
+                <div className="screen-sidebar-body">{sidebar}</div>
+              </div>
+            )}
           </div>
         </div>
 
-        {(footer || secondaryActions) &&
-        <div className="screen-footer-container">
-          {footer && <div className="screen-footer">{footer}</div>}
-          {secondaryActions && <div className="screen-secondary-actions">{secondaryActions}</div>}
-        </div>}
+        {(footer || secondaryActions)
+        && (
+          <div className="screen-footer-container">
+            {footer && <div className="screen-footer">{footer}</div>}
+            {secondaryActions && <div className="screen-secondary-actions">{secondaryActions}</div>}
+          </div>
+        )}
       </div>
     );
   }
@@ -127,13 +152,20 @@ class Screen extends React.Component {
 
 Screen.propTypes = {
   dispatch: PropTypes.func,
-  alerts: PropTypes.array,
+  alerts: PropTypes.array, // eslint-disable-line
   header: PropTypes.node,
-  breadcrumbs: PropTypes.array,
+  breadcrumbs: PropTypes.arrayOf(PropTypes.shape({
+    to: PropTypes.string,
+    text: PropTypes.string,
+  })),
   sidenav: PropTypes.node,
   sidenavHeader: PropTypes.oneOfType([PropTypes.node, PropTypes.bool]),
   sidebar: PropTypes.node,
-  tabs: PropTypes.array,
+  match: PropTypes.object.isRequired, // eslint-disable-line
+  tabs: PropTypes.arrayOf(PropTypes.shape({
+    to: PropTypes.string.isRequired,
+    text: PropTypes.string,
+  })),
   body: PropTypes.node,
   footer: PropTypes.node,
   primaryActions: PropTypes.node,
@@ -143,12 +175,18 @@ Screen.propTypes = {
 
 Screen.defaultProps = {
   alerts: [],
+  body: null,
   breadcrumbs: [],
   dispatch: () => {},
   footer: null,
+  header: null,
   maxWidth: '1008px',
+  primaryActions: null,
+  secondaryActions: null,
+  sidebar: null,
+  sidenav: null,
   sidenavHeader: null,
   tabs: [],
 };
 
-export default Screen;
+export default withRouter(Screen);
