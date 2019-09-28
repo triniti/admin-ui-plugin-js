9e6cdf2068b47b6b761a2d69fe12b2e417f067b1
Greg Brown <greg@12dnetworks.com>
2019-09-28 11:35:08 -0700

    prep for v1.0.0

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import reducer from './reducers';
 
 export default class AdminUiPlugin extends Plugin {
   constructor() {
-    super('triniti', 'admin-ui', '0.5.14');
+    super('triniti', 'admin-ui', '1.0.0');
   }
 
   configure(app, bottle) {
